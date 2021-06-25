d7189b659221828084ce3dbaae438f090ba2aec0
lterenty <86267006+lterenty@users.noreply.github.com>
2021-06-24 23:24:58 -0500

    Add files via upload

---
+++ CVS_String/CVS_String/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Data;
+using System.Collections.Generic;
+
+namespace CVS_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.Write("Enter filter: ");
+            string val = Console.ReadLine();
+            string file = File.ReadAllText("/Users/alexsherman/Projects/CVS_String/CVS_String/test.csv");
+           
+            string[] lines = file.Split(new string[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+            decimal Total = 0;
+
+            var dataObjects = new List<DataObject>();
+           
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                DataObject dataObj= new DataObject(lines[i]);
+                dataObjects.Add(dataObj);
+            }
+            for(int i = 0; i<dataObjects.Count; i++)
+            {
+                //if (dataObjects[i].LastName.Contains(val))
+                //{
+                //    Total = dataObjects[i].TransactionAmount++;
+                //    Console.WriteLine(dataObjects[i]);
+                //}
+                if (dataObjects[i].TransactionDate > DateTime.Parse(val))
+                {
+                    Total = dataObjects[i].TransactionAmount++;
+                    Console.WriteLine(dataObjects[i]);
+                }
+            }
+
+            Console.WriteLine("Total:" + Total);
+        }
+    }
+}
---
+++ DataObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace CVS_String
+{
+    public class DataObject
+    {
+        public string FullName;
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        public int TransactionNumber;
+        public decimal TransactionAmount;
+        public DateTime TransactionDate;
+
+        public DataObject(string rowdata)
+        {
+            string dateString = "1 February 2018";
+            DateTime startDate = DateTime.Parse(dateString);
+            var dateTime = DateTime.Parse(dateString);
+            string[] data = rowdata.Split(',');
+            
+            this.FullName = data[0].ToUpper();
+            var indexOfFirstSpace = FullName.IndexOf(" ");
+            this.FirstName = FullName.Substring(0, indexOfFirstSpace);
+            var second = FullName.Substring(indexOfFirstSpace + 1);
+            var indexOfSecondSpace = second.IndexOf(" ");
+            if (indexOfSecondSpace > 0)
+            {
+                this.MiddleName = second.Substring(0, indexOfSecondSpace);
+                this.LastName = second.Substring(indexOfSecondSpace + 1);
+            }
+            else
+            {
+                this.LastName = FullName.Substring(indexOfFirstSpace + 1);
+            }
+            this.TransactionNumber = Convert.ToInt32(data[1]);
+            this.TransactionAmount = decimal.Parse(data[2], System.Globalization.NumberStyles.Currency);
+            if (!string.IsNullOrEmpty(data[3]))
+            {
+                TimeSpan ts = new TimeSpan(Convert.ToInt32(data[3]), 0, 0, 0);
+                this.TransactionDate = startDate.Subtract(ts);
+            }
+            else
+            {
+                this.TransactionDate = DateTime.Now;
+            }
+           
+        }
+
+        public override string ToString()
+        {
+            string str = $"First Name: {FirstName} \n" +
+                 $"Middle Name: {MiddleName} \n" +
+                 $"Last Name: {LastName} \n" +
+                 $"Transaction Number: {TransactionNumber} \n" +
+                 $"Transaction Amount: {TransactionAmount} \n" +
+                 $"DateTme: {TransactionDate}";
+
+            return str;
+        }
+    }
+}
+
+
+
